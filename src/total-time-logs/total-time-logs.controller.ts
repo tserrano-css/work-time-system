7246ac1f014115f543e22625ba97016fe385e3ce
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2022-01-23 11:48:58 +0100

    Endpoint para descargar PDF

--- src/total-time-logs/total-time-logs.controller.ts
+++ src/total-time-logs/total-time-logs.controller.ts
@@ -1,19 +1,33 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Response, StreamableFile, UseGuards } from '@nestjs/common';
 import { TotalTimeLogsService } from './total-time-logs.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { AuthUser } from 'src/common/auth-user.decorator';
 import { User } from 'src/users/entities/user.entity';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @ApiTags('total-time-logs')
 @ApiBearerAuth('JWT')
-@Controller('total-time-logs')
+@Controller()
 @UseGuards(JwtAuthGuard)
 export class TotalTimeLogsController {
   constructor(private readonly totalTimeLogsService: TotalTimeLogsService) {}
 
-  @Get()
+  @Get('total-time-logs')
   findAll(@AuthUser() authUser: User) {
     return this.totalTimeLogsService.findAll(authUser);
+  }
+
+  @Get('total-time-logs-pdf')
+  getFilePdf(@Response({ passthrough: true }) res): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'documento.pdf'));
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachement; filename="document.pdf"',
+    });
+
+    return new StreamableFile(file);
   }
 }
